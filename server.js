fc6902204e5f77918d2f1b3cde11d3af06f8c000
milinthalekh1912 <milinthalekh1912@gmail.com>
2022-05-18 20:48:20 +0700

    make it better

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 let express = require('express')
+let app = express();
+let bodyParser = require('body-parser')
+let mysql = require('mysql')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended :true}))
